526273d6ec40fe5532d0aca65da19cdb879b48d7
varankeerthi-dev <varankeerthi@gmail.com>
2026-02-18 01:05:26 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,4 +2,6 @@ self.addEventListener("install", event => {
     self.skipWaiting();
   });
   
+
   self.addEventListener("fetch", event => {});
+const CACHE_NAME = "expense-app-v2"; // change v2 -> v3 every update
